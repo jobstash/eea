dc54cb43742129607b16cecd4a0bc8eddc4cb1d8
cristiano cardelli <cristianocardelli@gmail.com, cristiano@welance.com>
2025-02-21 11:51:21 +0100

    fix header title style

--- wp-content/themes/wp-starter/init/shortcodes.php
+++ wp-content/themes/wp-starter/init/shortcodes.php
@@ -42,3 +42,25 @@ function add_title_body_class($classes) {
 add_filter('body_class', 'add_title_body_class');
 
 add_action( 'init', 'register_shortcodes');
+
+function my_custom_mce_buttons($buttons) {
+  array_unshift($buttons, 'styleselect'); // Aggiunge il menu a tendina
+  return $buttons;
+}
+add_filter('mce_buttons', 'my_custom_mce_buttons');
+
+
+function my_custom_mce_formats($init_array) {
+  $style_formats = array(
+      array(
+          'title' => 'Gradient Text',
+          'inline' => 'span',
+          'classes' => 'gradient-text',
+      ),
+  );
+
+  $init_array['style_formats'] = json_encode($style_formats);
+  return $init_array;
+}
+add_filter('tiny_mce_before_init', 'my_custom_mce_formats');
+
