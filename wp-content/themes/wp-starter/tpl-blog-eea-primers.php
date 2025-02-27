927c3be691344e4d6d518776093d2148349c2dcb
cristiano cardelli <cristianocardelli@gmail.com, cristiano@welance.com>
2025-02-27 13:20:41 +0100

    fix 404 and text pages

--- wp-content/themes/wp-starter/tpl-blog-eea-primers.php
+++ wp-content/themes/wp-starter/tpl-blog-eea-primers.php
@@ -1,5 +1,6 @@
 <?php
 /* Template Name: EEA Primers Category Page */
+
 $context = Timber::context();
 $context['post'] = Timber::get_post();
 $context['title'] = get_the_title();
@@ -9,6 +10,7 @@ $context['posts'] = Timber::get_posts([
   'post_type' => 'post',
   'category_name' => 'eea-primers',
   'posts_per_page' => 9, // or specify a number like 10
+  'paged'          => $paged,
 ]);
 
 Timber::render('page-blog.twig', $context);
--- wp-content/themes/wp-starter/tpl-blog-members-spotlight.php
+++ wp-content/themes/wp-starter/tpl-blog-members-spotlight.php
@@ -9,6 +9,7 @@ $context['posts'] = Timber::get_posts([
   'post_type' => 'post',
   'category_name' => 'members-spotlight',
   'posts_per_page' => 9, // or specify a number like 10
+  'paged'          => $paged,
 ]);
 
 Timber::render('page-blog.twig', $context);
